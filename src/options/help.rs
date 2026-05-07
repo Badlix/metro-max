5b1d35e27998a9862d5ce914ca1f4604c394f205
Badlix <mathieu.leroux173@gmail.com>
2026-05-07 11:25:33 +0200

    feat: add clap crate

--- src/options/help.rs
+++ src/options/help.rs
@@ -1,11 +1,18 @@
 use colored::Colorize;
 
+// TODO : unused for now, to reimplement if clap can't display custom colorful output
+
+#[allow(dead_code)]
 pub fn help() -> usize {
     println!();
     println!("{}", "A rust CLI for music !".blue());
     println!();
     println!("{}", "Options : ".green());
-    println!("\t-h : this message, use metromax [command] -h, to have more detail explanation on a certain command");
+    println!("\t-h, --help : this message, use metromax [option] -h, to have more detail explanation on a certain option");
+    println!("\t-v, --version : display current version");
+    println!();
+    println!("{}", "Commands : ".green());
+    println!("\tlaunch [bpm] : launch a metronom with a given bpm value");
     println!();
     1
 }
--- src/options/version.rs
+++
@@ -1,6 +0,0 @@
-use std::env;
-
-pub fn version() -> usize {
-    println!("Version {}", env!("CARGO_PKG_VERSION"));
-    1
-}
