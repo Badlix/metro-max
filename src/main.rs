d7b491352d8e5e880114c26b97206317eeb1da78
Mathieu Leroux <113862047+Badlix@users.noreply.github.com>
2026-05-11 13:26:39 +0200

    Feat/add start command (#4)
    feat(start): add command to start the metronome

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 use colored::Colorize;
 use std::env;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 mod options;
+mod commands;
 
 #[derive(Parser)]
 #[command(version, about = "A rust CLI for music !")]
-struct SimpleCLI {}
+struct SimpleCLI {
+    #[command(subcommand)]
+    command: CommandsEnum
+}
+
+#[derive(Subcommand)]
+enum CommandsEnum {
+    Start(commands::start::StartArgs)
+}
 
 fn main() {
-    SimpleCLI::parse();
+    let cli = SimpleCLI::parse();
 
     let args: Vec<String> = env::args().collect();
     
     if args.len() == 1 {
         println!("{}", "METROMAX".blue().bold());
         println!("\n{}", "See --help".blue().bold());
+    }
+
+    match &cli.command {
+        CommandsEnum::Start(args) => {
+            commands::start::start(args.bpm, args.beat_number, args.big_key_index.clone());
+        }
     }
 }
