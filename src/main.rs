af5daf8d8331fb9e16ad0cb94dbbbca6259a6ade
Mathieu Leroux <113862047+Badlix@users.noreply.github.com>
2026-05-11 13:24:11 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,6 @@ enum CommandsEnum {
     Start(commands::start::StartArgs)
 }
 
-#[derive(Parser)]
-#[command(version, about = "A rust CLI for music !")]
-struct SimpleCLI {}
-
 fn main() {
     let cli = SimpleCLI::parse();
 
